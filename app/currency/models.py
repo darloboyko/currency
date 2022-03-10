1c82ece5ac1f668cd77274acbba4966fe2fd014f
darloboyko <darloboyko@gmail.com>
2022-03-10 12:17:56 +0200

    first migration

---
+++ app/currency/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=15)
+    subject = models.CharField(max_length=25)
+    message = models.CharField(max_length=254)
